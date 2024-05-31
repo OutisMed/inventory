3a1a7125d710654511ef470e330281475de05d95
OutisMed <medardkik01@gmail.com>
2024-05-31 11:15:09 +0200

    resoudre le problème de version

--- inventory/app/build.gradle.kts
+++ inventory/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -50,6 +51,15 @@ android {
 }
 
 dependencies {
+
+
+    //Room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // To use Kotlin Symbol Processing (KSP)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- inventory/app/src/main/java/com/example/inventory/data/Item.kt
+++ inventory/app/src/main/java/com/example/inventory/data/Item.kt
@@ -17,10 +17,14 @@
 package com.example.inventory.data
 
 
+
+
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
